c5e24454a7701968879d9ff7b49f53a6d0145a2f
Nitish <njaswal.nitish@gmail.com>
2021-04-19 23:58:22 +0530

    testing packages

---
+++ lib/screens/homescreen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class homescreen extends StatefulWidget{
+  @override
+  _homeScreenState createState()=> _homeScreenState();
+}
+
+
+class _homeScreenState extends  State<homescreen>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+
+
+}
